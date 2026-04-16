bf5fa771a2eed1bb79fcfec5a697cf7b746a0e91
Akhil Zubair <148029061+Akhhi@users.noreply.github.com>
2026-04-16 12:12:17 +0530

    chore: remove obsolete test script for Firestore data fetching

--- test.js
+++
@@ -1,8 +0,0 @@
-const src = 'https://firestore.googleapis.com/v1/projects/opema-clothing/databases/(default)/documents/artifacts/default-app-id/public/data/products';
-fetch(src).then(r => r.json()).then(data => {
-    const id = data.documents[0].name.split('/').pop();
-    console.log('ID:', id);
-    fetch(src + '/' + id).then(r => r.json()).then(d => {
-        console.log(JSON.stringify(d.fields, null, 2));
-    });
-});
